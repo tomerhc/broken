fb5f64f5bb37ac23f8d516295c5237846de9913f
tomerh <hacohen0tomer@gmail.com>
2020-05-29 23:35:41 +0300

    implementing fiestel rounds and key padding functions. deryption still not working

--- src/feistel.rs
+++ src/feistel.rs
@@ -5,12 +5,9 @@ pub fn encrypt(mut msg: Vec<u8>, mut key: Vec<u8>, rounds: i32) -> Result<Vec<u8
     pad(&mut msg, 128);
     pad_key(&mut key, 64);
     for _ in 0..rounds {
-        println!("msg {:?}\n{}", msg, msg.len());
-        println!("key {:?}\n{}", key, key.len());
         fiestel_round(&mut msg, &key)?;
         inc_key(&mut key);
     }
-    swap(&mut msg);
     Ok(msg)
 }
 
@@ -18,13 +15,12 @@ pub fn decrypt(mut msg: Vec<u8>,  mut key: Vec<u8>, rounds: i32) -> Result<Vec<u
     //assertions
     pad_key(&mut key, 64);
     swap(&mut msg);
-    println!("msg {:?}\n{}", msg, msg.len());
-    println!("key {:?}\n{}", key, key.len());
-    let mut final_key = calc_final_key(&key, rounds);
+    let mut final_key = calc_final_key(&key, rounds-1);
     for _ in 0..rounds {
         fiestel_round(&mut msg, &final_key)?;
         dec_key(&mut final_key);
     }
+    swap(&mut msg);
     Ok(msg)
 }
 
@@ -33,14 +29,11 @@ pub fn fiestel_round<'a >(msg: &'a mut Vec<u8>, k: &'a Vec<u8>) -> Result<(), En
     assert!(msg.len() == 128, "msg should be 2X256bits / 128 bytes");
     assert!(k.len() == 64, "key should be 256bits / 64 bytes");
     
-    let right = msg.split_off(msg.len()/2);
-    println!("right {:?}\n{}", right, right.len());
-    println!("left {:?}\n{}", msg, msg.len());
-    let mut f_of_right = f_func(&mut right.clone(), k)?;
-    println!("f(right) {:?}\n{}", f_of_right, f_of_right.len());
+    let mut right = msg.split_off(msg.len()/2);
+    let f_of_right = f_func(&mut right.clone(), k)?;
     msg.iter_mut().zip(f_of_right.iter()).for_each(|(x1, x2)| *x1 ^= *x2);
-    f_of_right.append(msg);
-    *msg = f_of_right;
+    right.append(msg);
+    *msg = right;
     Ok(())
 }
 
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ fn main(){
     let key = String::from("secret");
     let msg_bytes = text.into_bytes();
     let key_bytes = key.into_bytes();
-    let cypher = feistel::encrypt(msg_bytes, key_bytes.clone(), 1).unwrap();
-    let dec = feistel::decrypt(cypher, key_bytes, 1).unwrap();
-    println!("{:?}", String::from_utf8(dec));
+    let cypher = feistel::encrypt(msg_bytes, key_bytes.clone(), 5).unwrap();
+    println!("cypher text: {:?}\n\n", String::from_utf8(cypher.clone()).unwrap());
+
+    let dec = feistel::decrypt(cypher, key_bytes, 5).unwrap();
+    let dec_str = String::from_utf8(dec).unwrap();
+    let dec_str = dec_str.trim_matches('\x00');
+    println!("decrypted: {:?}", dec_str);
 
 }
 
