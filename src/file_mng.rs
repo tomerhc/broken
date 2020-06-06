d4f76e1d4d29c5f711c9bf04c7d5f984f1626bcb
tomerh <hacohen0tomer@gmail.com>
2020-06-06 17:39:57 +0300

    added file reading, encryption and decryption functinality

---
+++ src/file_mng.rs
@@ -0,0 +1,77 @@
+use std::fs::{File, metadata};
+use std::io::prelude::*;
+use std::mem;
+use byteorder::{LittleEndian, WriteBytesExt, ReadBytesExt};
+
+use crate::counter_block;
+
+
+pub fn read_clear_file(path: &str) -> std::io::Result<Vec<u8>>{
+    let mut f = File::open(path)?;
+    let meta = metadata(path)?;
+    let mut buff = vec![0u8;meta.len() as usize];
+    f.read(&mut buff)?;
+    Ok(buff)
+}
+
+pub fn read_enc_file(path: &str) -> std::io::Result<counter_block::Blocks>{
+    // assertions
+    let mut f = File::open(path).unwrap();
+    let mut block_size_buff = [0u8;mem::size_of::<i32>()];
+    let mut nonce_size_buff = [0u8;mem::size_of::<i32>()];
+    let mut rounds_num_buff = [0u8;mem::size_of::<i32>()];
+
+    f.read(&mut block_size_buff)?;
+    f.read(&mut nonce_size_buff)?;
+    f.read(&mut rounds_num_buff)?;
+
+    let block_size: i32 = i32::from_le_bytes(block_size_buff);
+    let nonce_size: i32 = i32::from_le_bytes(nonce_size_buff);
+    let rounds_num: i32 = i32::from_le_bytes(rounds_num_buff);
+    let rest_of_file = f.metadata().unwrap().len() as usize - (3 * mem::size_of::<i32>()) - nonce_size as usize;
+    let mut nonce = vec![0u8; nonce_size as usize];
+    f.read(&mut nonce)?;
+
+    let mut blocks: Vec<Vec<u8>> = Vec::with_capacity(rest_of_file / block_size as usize);
+    for _ in 0..blocks.capacity(){
+        let mut buff = vec![0u8; block_size as usize];
+        f.read(&mut buff)?;
+        blocks.push(buff);
+    }
+    
+    Ok(
+        counter_block::Blocks {
+            nonce: nonce,
+            f_rounds: rounds_num,
+            blocks: blocks
+        }
+    )
+}
+
+
+pub fn write_blocks(mut cypher: counter_block::Blocks, path: &str) -> std::io::Result<()>{
+    //assertions
+
+    let block_size = cypher.blocks[0].len();
+    let byte_size = cypher.blocks.len() * block_size;
+    let nonce_size = cypher.nonce.len();
+    let mut serial_rounds_num = [0u8;mem::size_of::<i32>()];
+    serial_rounds_num.as_mut().write_i32::<LittleEndian>(cypher.f_rounds).expect("could not write num rounds");
+    let mut serial_block_size = [0u8;mem::size_of::<i32>()];
+    serial_block_size.as_mut().write_i32::<LittleEndian>(block_size as i32).expect("could not write block size");
+    let mut serial_nonce_size = [0u8;mem::size_of::<i32>()];
+    serial_nonce_size.as_mut().write_i32::<LittleEndian>(nonce_size as i32).expect("could not write nonce size");
+    let mut write_buff: Vec<u8> = Vec::with_capacity(nonce_size + byte_size + serial_rounds_num.len());
+    write_buff.append(&mut serial_block_size.to_vec());
+    write_buff.append(&mut serial_nonce_size.to_vec());
+    write_buff.append(&mut serial_rounds_num.to_vec());
+    write_buff.append(&mut cypher.nonce);
+    for b in cypher.blocks.iter_mut(){
+        write_buff.append(b)
+    }
+    let mut enc_file = File::create(path)?;
+    enc_file.write_all(&mut write_buff)?;
+    Ok(())
+}
+
+
--- src/main.rs
+++ src/main.rs
@@ -5,44 +5,22 @@ use std::env::args;
 mod hasher;
 mod feistel;
 mod counter_block;
+mod file_mng;
 
 
 fn main() -> std::io::Result<()>{
     let args: Vec<String> = args().collect();
-    let mut path = args[1].to_owned();
+    let path = args[1].to_owned();
     let pass = args[2].to_owned().into_bytes();
-    let mut file = File::open(&path)?;
-    let meta = metadata(&path)?;
-    let mut buff = vec![0u8;meta.len() as usize];
-    file.read(&mut buff)?;
+    let file_buff = file_mng::read_clear_file(&path)?;
+    let cypher = counter_block::encrypt(file_buff, pass.clone(), 15, 5).unwrap();
 
-    let mut cypher = counter_block::encrypt(buff, pass, 15, 5).unwrap();
-    let mut write_buff: Vec<u8> = Vec::new();
-    write_buff.append(&mut cypher.nonce);
-    for b in cypher.blocks.iter_mut(){
-        write_buff.append(b);
-    }
-    path.push_str("_enc");
-    let mut enc_file = File::create(path)?;
-    enc_file.write_all(&mut write_buff)?;
-    Ok(())
-}
-
-
-
-fn old_main(){
-    let text = String::from("hello wolrd, ny name is tomer and i am here to write an encryption softwere and save the world from slow enryption.");
-    let bytes = text.into_bytes();
-    let key = String::from("Barvaz1");
-    let key_bytes = key.into_bytes();
+    file_mng::write_blocks(cypher, r"c:\Users\hacoh\desktop\test_file.txt_enc")?;
+    let b = file_mng::read_enc_file(&path)?;
     
-    let cypher = counter_block::encrypt(bytes, key_bytes.clone(), 15, 3).unwrap();
-    println!("nonce => {:?}\n", cypher.nonce);
-    for b in cypher.blocks.iter(){
-        println!("{:?}", String::from_utf8(b.to_vec()))
-    }
-    let dec = counter_block::decrypt(cypher, key_bytes).unwrap();
+    let dec = counter_block::decrypt(b, pass).unwrap();
     let dec_str = String::from_utf8(dec).unwrap();
     let dec_str = dec_str.trim_matches('\x00');
-    println!("decrypted => {:?}", dec_str)
+    println!("decrypted => {:?}", dec_str);
+    Ok(())
 }
